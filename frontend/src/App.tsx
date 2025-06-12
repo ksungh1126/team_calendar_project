5b6a0f00f469a3563f9ca21345290321eafeebc0
ksungh1126 <dama031126@naver.com>
2025-06-13 08:44:00 +0900

    수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from '@mui/material/styles';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { ko } from 'date-fns/locale';
+
+import theme from './theme';
+import { AuthProvider } from './context/AuthContext';
+import { EventProvider } from './context/EventContext';
+import { TeamProvider } from './context/TeamContext';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
@@ -9,21 +18,38 @@ import TeamspacePage from './pages/TeamspacePage';
 import FriendPage from './pages/FriendPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5분
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
-          <Route path="/team" element={<ProtectedRoute><TeamPage /></ProtectedRoute>} />
-          <Route path="/teamspace/:teamId" element={<ProtectedRoute><TeamspacePage /></ProtectedRoute>} />
-          <Route path="/friend" element={<ProtectedRoute><FriendPage /></ProtectedRoute>} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ko}>
+          <AuthProvider>
+            <TeamProvider>
+              <EventProvider>
+                <Router>
+                  <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
+                    <Route path="/team" element={<ProtectedRoute><TeamPage /></ProtectedRoute>} />
+                    <Route path="/teamspace/:teamId" element={<ProtectedRoute><TeamspacePage /></ProtectedRoute>} />
+                    <Route path="/friend" element={<ProtectedRoute><FriendPage /></ProtectedRoute>} />
+                  </Routes>
+                </Router>
+              </EventProvider>
+            </TeamProvider>
+          </AuthProvider>
+        </LocalizationProvider>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
