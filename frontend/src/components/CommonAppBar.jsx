94d4480443279b004f8acf4fa87d56a578a4a961
Juyong-Park202201523 <jyp2092@hufs.ac.kr>
2025-06-13 04:39:28 +0900

    Update CommonAppBar.jsx

--- frontend/src/components/CommonAppBar.jsx
+++ frontend/src/components/CommonAppBar.jsx
@@ -14,8 +14,8 @@ const CommonAppBar = ({ userName = 'user', pageName = '' }) => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('user'); // 로그인 정보 제거
-    window.location.href = 'http://localhost:4000/'; // 로그인 페이지로 이동 (브라우저 새로고침)
+    localStorage.removeItem('user');
+    navigate('/login');
 };
 
   return (
